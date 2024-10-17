3aa29f2829cfadb3e416a74c989190c5a80bc827
leowjli <leo.wj.li@gmail.com>
2024-10-17 13:08:47 -0700

    set up MVC and tailwind

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjYRvRQyyjJORIqi-wFtsZOD45sPesaOo",
+  authDomain: "daymax-86edc.firebaseapp.com",
+  projectId: "daymax-86edc",
+  storageBucket: "daymax-86edc.appspot.com",
+  messagingSenderId: "1076553295004",
+  appId: "1:1076553295004:web:7d726d496f81062e277959"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +25,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
