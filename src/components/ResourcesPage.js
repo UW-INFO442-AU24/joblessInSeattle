d6284965e63be12788cc782cd89bd34fefcda93c
Fan Bu <fannbuu@gmail.com>
2024-11-07 13:39:34 -0800

    not important

--- src/components/ResourcesPage.js
+++ src/components/ResourcesPage.js
@@ -1,6 +1,9 @@
 function ResourcesPage() {
     return (
         <div>
+            <main className="ResourcesPage">
+                <h1>This is ResourcesPage</h1>
+            </main>
         </div>
     )
 }
