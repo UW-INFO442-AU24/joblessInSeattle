fdeb8f2e92f99340ee78e727377f9f0ec99687bd
emilychoi <eemilychoi@gmail.com>
2024-11-07 17:07:37 -0800

    cards display in 2x2 grid when small

--- src/frontend/Dashboard.js
+++ src/frontend/Dashboard.js
@@ -4,28 +4,22 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export default function Dashboard() {
-
-    const healthCategories = ['Water', 
-                              'Sleep', 
-                              'Medications', 
-                              'My Buddy'];
-    const dashboardCards = healthCategories.map((category) => 
-          <Row xs={2} md={2} className="g-4">
-            <Col>
-              <Card>
-                <Card.Body>
-                  <Card.Title>{category}</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-        </Row>
-    )
   return (
-    dashboardCards
+    <Row xs={2} md={2} className="g-4">
+      {Array.from(['Water', 'Sleep', 'Medications', 'My Buddy']).map((category) => (
+        <Col key={category}>
+          <Card>
+            <Card.Body>
+              <Card.Title>{category}</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
