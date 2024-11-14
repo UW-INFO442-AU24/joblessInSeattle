0c29760c010c5dcd4033437859e1e7efbd55b58b
ydodobara <ydodo@uw.edu>
2024-11-13 23:28:48 -0800

    added filler text for boxes in sleeptracker

--- src/frontend/sleepTracker.js
+++ src/frontend/sleepTracker.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import { ButtonGroup } from 'react-bootstrap';
 
 export default function sleepTracker() {
 
@@ -15,8 +16,10 @@ export default function sleepTracker() {
                 <Card>
                     <Card.Body>
                         <Card.Title>Going to Bed?</Card.Title>
-                        <Button variant='primary'>Bed time :D</Button>
-                        <Button variant='success'>Wakey wakey</Button>
+                        <ButtonGroup vertical>
+                            <Button variant='primary' className='my-2'>Bed time :D</Button>
+                            <Button variant='success' className='my-2'>Wakey wakey</Button>
+                        </ButtonGroup>
                     </Card.Body>
                 </Card>
             </Row>
@@ -25,12 +28,11 @@ export default function sleepTracker() {
             <Row className='m-4'>
                 <Card>
                     <Card.Body>
-                    <Card.Title>Your Sleep</Card.Title>
-                    <Card.Text>
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
-                    </Card.Text>
+                        <Card.Title>Your Sleep</Card.Title>
+                        <Card.Text>
+                            Bar chart for single day and line chart for the others?
+                            MUI X has bar and line chart components that can be used
+                        </Card.Text>
                     </Card.Body>
                 </Card>
             </Row>
@@ -40,6 +42,9 @@ export default function sleepTracker() {
                 <Card>
                     <Card.Body>
                         <Card.Title>Sleep Goals</Card.Title>
+                        <Card.Text>Current Goal: 8 hr 00 min</Card.Text>
+                        <Card.Text>You've reached it X% of times</Card.Text>
+                        <Card.Text>Icon somewhere to edit goal</Card.Text>
                     </Card.Body>
                 </Card>
             </Row>
@@ -49,6 +54,8 @@ export default function sleepTracker() {
                 <Card>
                     <Card.Body>
                         <Card.Title>Buddy's Sleep</Card.Title>
+                        <Card.Text>Buddy's health in relation to your sleep</Card.Text>
+                        <Card.Text>Maybe some sort of status bar or something</Card.Text>
                     </Card.Body>
                 </Card>
             </Row>
