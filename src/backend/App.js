9c9f7ae15b8941135bcabb0d42caa09251edb536
emilychoi <eemilychoi@gmail.com>
2024-11-14 12:48:07 -0800

    water tracker html updated

--- src/backend/App.js
+++ src/backend/App.js
@@ -17,3 +17,19 @@ const App = ({ auth }) => {
 };
 
 export default App;
+import React from "react";
+//import Dashboard from '../frontend/Dashboard.js';
+//<Dashboard></Dashboard>
+import WaterTracker from "../frontend/WaterTracker";
+//<WaterTracker></WaterTracker>
+
+
+const App = ({ auth }) => {
+    return (
+        <div>
+            <WaterTracker></WaterTracker>
+        </div>
+    );
+};
+
+export default App;
