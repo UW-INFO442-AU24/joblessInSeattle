c9a587780c4bbb025616329f808ec714343fb68f
emilychoi <eemilychoi@gmail.com>
2024-11-14 14:36:24 -0800

    added react routing to app.js, changed dashboard laylout

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 //import Dashboard from '../frontend/Dashboard.js';
 //<Dashboard></Dashboard>
-import WaterTracker from "../frontend/WaterTracker";
+//import WaterTracker from "../frontend/WaterTracker";
 //<WaterTracker></WaterTracker>
 
+//import components
+import Dashboard from "../frontend/Dashboard"
+import WaterTracker from "../frontend/WaterTracker"
 
-const App = ({ auth }) => {
+function App() {
     return (
-        <div>
-            <WaterTracker></WaterTracker>
-        </div>
-    );
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={ <Dashboard/> } />
+                        <Route path="watertracker" element={ <WaterTracker/> } />
+                        {/* <Route path="contact" element={ <Contact/> } /> */}
+                    </Routes>
+               </BrowserRouter>
+            </div>
+    )
 };
 
 export default App;
--- src/frontend/Dashboard.js
+++ src/frontend/Dashboard.js
@@ -2,24 +2,79 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <Row xs={2} md={2} className="g-4">
-      {Array.from(['Water', 'Sleep', 'Medications', 'My Buddy']).map((category) => (
-        <Col key={category}>
-          <Card>
-            <Card.Body>
-              <Card.Title>{category}</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
+    <div>
+        <Col className='m-4' >
+            <h1>MY DASHBOARD</h1>
+
+            {/* WATER TRACKER */}
+            <Row className='m-4'>
+                <Card>
+                  <Card.Body>
+                    <Card.Title><Link to="/watertracker">Water</Link></Card.Title>
+                      <Card.Text>
+                          16/64 fl oz.
+                      </Card.Text>
+                  </Card.Body>
+                </Card>
+            </Row>
+                
+            {/* SLEEP TRACKER */}
+            <Row className='m-4'>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>Sleep</Card.Title>
+                      <Card.Text>
+                          placeholder for amount of sleep today.
+                      </Card.Text>
+                  </Card.Body>
+                </Card>
+            </Row>
+
+            {/* MEDICATIONS */}
+            <Row className='m-4'>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>Medications</Card.Title>
+                      <Card.Text>
+                          allergy pills!
+                      </Card.Text>
+                  </Card.Body>
+                </Card>
+            </Row>
+
+            {/* MY BUDDY */}
+            <Row className='m-4'>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>My Buddy</Card.Title>
+                      <Card.Text>
+                        :D
+                      </Card.Text>
+                  </Card.Body>
+                </Card>
+            </Row>
         </Col>
-      ))}
-    </Row>
+    </div>
+
+  //   <Row xs={2} md={2} className="g-4">
+  //     {Array.from(['Water', 'Sleep', 'Medications', 'My Buddy']).map((category) => (
+  //       <Col key={category}>
+  //         <Card>
+  //           <Card.Body>
+  //             <Card.Title><Link to="/watertracker">{category}</Link></Card.Title>
+  //             <Card.Text>
+  //               This is a longer card with supporting text below as a natural
+  //               lead-in to additional content. This content is a little bit
+  //               longer.
+  //             </Card.Text>
+  //           </Card.Body>
+  //         </Card>
+  //       </Col>
+  //     ))}
+  //   </Row>
   );
 }
