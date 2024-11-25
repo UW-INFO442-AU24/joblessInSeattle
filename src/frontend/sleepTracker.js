7d746ad7bddc9c5da4fe9b26883b1abd19525f12
ydodobara <ydodo@uw.edu>
2024-11-24 21:25:18 -0800

    updated sleeptracker.js

--- src/frontend/sleepTracker.js
+++ src/frontend/sleepTracker.js
@@ -25,6 +25,45 @@ function TimeInputs(props) {
 
 export default function SleepTracker() {
 
+    const [clicked, setClicked] = useState(false);
+    const [value, setValue] = useState(false);
+    const [edit, setEdit] = useState(false);
+
+    const handleClick = (event) =>{
+
+        const clickTime = new Date();
+        setClicked(true);
+        console.log(clickTime);
+        // send clickTime to DB and then after setClicked(false)
+    }
+
+    // Submit time inputted by user
+    const handleSubmit = (event) => {
+
+    }
+
+    const enableEdit = (event) => {
+
+        setEdit(true);
+    }
+
+    const disableEdit = (event) => {
+        setEdit(false);
+    }
+
+    // Check value of inputs to make sure response is within time
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value);
+        console.log(value);
+        if (value < 60 || value > 0) {
+            setValue(value);
+        }
+        else {
+            throw new Error("Value is not within time");
+        }
+
+    }
+
     return (
 
         <div>
@@ -36,8 +75,8 @@ export default function SleepTracker() {
                         <Card.Body>
                             <Card.Title>Going to Bed?</Card.Title>
                             <ButtonGroup>
-                                <Button variant='primary' className='me-4 my-2'>Bed time</Button>
-                                <Button variant='success' className='ms-4 my-2'>Awake!</Button>
+                                <Button variant='primary' className='me-4 my-2' onClick={handleClick}>Bed time</Button>
+                                <Button variant='success' className='ms-4 my-2' onClick={handleClick}>Awake!</Button>
                             </ButtonGroup>
                             <Form>
                                 <Form.Group className="mb-3" controlId="sleepUserInput">
@@ -47,7 +86,7 @@ export default function SleepTracker() {
                                         <TimeInputs label='Wake-up time'/>
                                     </LocalizationProvider>
                                 </Form.Group>
-                                <Button variant='secondary'>Log</Button>
+                                <Button variant='secondary' onClick={handleSubmit}>Log</Button>
                             </Form>
                         </Card.Body>
                     </Card>
@@ -77,23 +116,40 @@ export default function SleepTracker() {
 
                 {/* Goal setting for sleep hours? */}
                 <Row className='m-4'>
-                    <Card>
+                    <Card className='mb-5'>
                         <Card.Body>
                             <Card.Title>Sleep Goals</Card.Title>
                             <Card.Text>Current Goal: 8 hr 00 min</Card.Text>
                             <Card.Text>You've reached it X% of times</Card.Text>
-                            <Button variant='secondary'>Edit Goal</Button>
-                        </Card.Body>
-                    </Card>
-                </Row>
-
-                {/* Overall trends or buddy's level of sleep relative to yours? */}
-                <Row className='m-4'>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Buddy's Sleep</Card.Title>
-                            <Card.Text>Buddy's health in relation to your sleep</Card.Text>
-                            <Card.Text>Maybe some sort of status bar or something</Card.Text>
+                            <Button variant='warning' className='my-2' onClick={enableEdit}>Edit Goal</Button>
+                            <Form>
+                                <Row className='my-2'>
+                                    <Col>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="hr"
+                                            aria-label="Disabled input example"
+                                            disabled={!edit}
+                                        />
+                                    </Col>
+                                    <Col>
+                                        hrs
+                                    </Col>
+                                    <Col>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="min"
+                                            aria-label="Disabled input example"
+                                            disabled={!edit}
+                                            onChange={handleChange}
+                                        />
+                                    </Col>
+                                    <Col>
+                                        min
+                                    </Col>
+                                </Row>
+                                <Button variant='success' className='my-2' onClick={disableEdit}>Submit</Button>
+                            </Form>
                         </Card.Body>
                     </Card>
                 </Row>
