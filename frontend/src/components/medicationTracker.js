d42b288c686e44174e386531edf8a40a85ad931b
Trevor Wong <tjwong22@uw.edu>
2024-12-01 20:16:02 -0800

    worked a bit on medications page

--- frontend/src/components/medicationTracker.js
+++ frontend/src/components/medicationTracker.js
@@ -5,44 +5,72 @@ import { LineChart } from '@mui/x-charts';
 import { fetchJSON } from "./utils.js";
 
 // water tracker
-function Counter() {
+function MedicationList() {
    const [count, setCount] = useState(0);
    const increment = () => setCount(count + 8);
    const decrement = () => setCount(count - 8);
 
    const reset = () => setCount(0);
 
-   const saveWaterInfo = async () => {
-        let waterIntake = {count}.count;
+   
+
+    return (
+        // return the medication list FULL card
+        <div>
+            <MedicationLine />
+        </div>
+    );
+}
+
+// function to return the html of what a single line of medication looks like
+function MedicationLine () {
+    return(
+        <div className='d-flex flex-row justify-content-between border-bottom'>
+            <div className='px-2'>
+                {/* left side of card */}
+                <div className='d-flex flex-row justify-content-between mt-1'>
+                    <p className='fw-bold mb-0'>Zyrtec</p>
+                    <p className='m-0'>1x</p>
+                </div>
+                <p className='text-black-50'>Allergy Medication RAH IM ADDING MORE TEXT TO THIS</p>
+            </div>
+            <div>
+                <button className="btn btn-primary">Status</button>
+            </div>
+        </div>
+        
+    )
+}
+
+function AddNewMedication () {
+
+    const addNewMedication = async () => {
+        let medicationName = document.getElementById('medicationNameInput');
+        let medDescription = document.getElementById('medDescriptionInput');
+        let medFrequency = document.getElementById('medFrequencyInput');
         try {
-            console.log(waterIntake)
             // CHANGE LATER -- NEEDS TO NOT BE ABSOLUTE URL PATH
-            await fetchJSON("http://localhost:3001/api/water", {
+            await fetchJSON("http://localhost:3001/api/medication", {
                 method: "POST",
-                body: { water: waterIntake }
+                body: { water : 3}
             })
-            reset();
 
         } catch (error) {
             console.error("Error saving count:", error);
         }
     };
 
-   return (
-       <div>
-            <div>
-                <Button onClick={decrement} disabled={count<=0 ? true : false}>-</Button>
-                <span id="waterCounter" class="mx-3 pt-2">{count} fl oz</span>
-                <Button variant='primary' onClick={increment}>+</Button>
-            </div>
-               
-           <div>
-               <Button variant='primary' className="mt-3" disabled={count<=0 ? true : false} onClick={saveWaterInfo}>
-                    Submit
-                </Button>
-           </div>
-       </div>
-   );
+    const showInputs = <div><input>Hey!</input></div>
+
+    return(
+        <div>
+            {/* onClick={showInputs} */}
+            <Button variant='primary' className="mt-3" >
+                Add Medication
+            </Button>
+            <div id="medication-input"></div>
+        </div>
+    )
 }
 
 export default function WaterTracker() {
@@ -71,8 +99,9 @@ export default function WaterTracker() {
                     <Card>
                         <Card.Body>
                         {/* <Card.Body class="text-center"> */}
-                            <Card.Title>Water Intake Input</Card.Title>
-                            <Counter />
+                            <Card.Title>Medication List</Card.Title>
+                            <MedicationList />
+                            <AddNewMedication />
                         </Card.Body>
                     </Card>
                 </Row>
@@ -80,46 +109,14 @@ export default function WaterTracker() {
                 <Row className='m-4'>
                     <Card>
                         <Card.Body>
-                            <Card.Title>Water Consumed Today</Card.Title>
-                            {/* call funciton to load water info? */}
+                            <Card.Title>Some other Card</Card.Title>
+                            {/* call funciton to load card info? */}
                         </Card.Body>
                     </Card>
                 </Row>
 
                     
-                {/* WATER DRANK THIS WEEK */}
-                <Row className='m-4'>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>This Week</Card.Title>
-                            <LineChart
-                                xAxis={[{ 
-                                    scaleType: 'time',
-                                    data: [new Date('2024-11-11'), new Date('2024-11-12'), new Date('2024-11-13'), new Date('2024-11-14'), new Date('2024-11-15'), new Date('2024-11-16'), new Date('2024-11-17')] 
-                                }]}
-                                series={[
-                                    {
-                                    data: [60, 52, 86, 75, 59, 68, 64]
-                                    },
-                                ]}
-                                width={200}
-                                height={200}
-                            />
-                        </Card.Body>
-                    </Card>
-                </Row>
-
-                {/* WATER GOALS*/}
-                <Row className='m-4'>
-                    <Card className='mb-5'>
-                        <Card.Body>
-                            <Card.Title>Water Goals</Card.Title>
-                            <Card.Text>
-                                this is a placeholder for the user to input their water goal for the day/week?
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Row>
+                
             </Col>
             <NavBar></NavBar>
         </div>
