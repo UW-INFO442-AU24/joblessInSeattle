022efed9e91ec88df682883d3f86df50baf056d9
ydodobara <ydodo@uw.edu>
2024-12-03 01:25:29 -0800

    can now post to sleep schema

--- backend/routes/controllers/sleep.js
+++ backend/routes/controllers/sleep.js
@@ -15,13 +15,40 @@ router.get('/', async (req, res) => {
 
 // POST to /sleep
 router.post('/', async (req, res) => {
-    try {
-      // stuff
-    }
-    catch(error) {
-      console.log("error: ", error);
-      res.status(500).json({status: "error", error: error});
-    }
+  try {
+    const newSleep = new req.models.SleepStats({
+      bedTime: req.body.bedTime,
+      wakeTime: req.body.wakeTime
+    });
+    console.log("this is before the save");
+    await newSleep.save();
+    console.log(req.body.bedTime);
+    console.log("this is after the save");
+    res.json({"status" : "success"});
+  }
+  catch(error) {
+    console.log("error: ", error);
+    res.status(500).json({status: "error", error: error});
+  }
 });
+
+// POST to /sleep
+// router.post('/bedTime', async (req, res) => {
+//     try {
+//       const newSleep = new req.models.SleepStats({
+//         date: Date.now(),
+//         bedTime: req.body.bedTime,
+//       });
+//       console.log("this is before the save");
+//       await newSleep.save()
+//       console.log(req.body.bedTime)
+//       console.log("this is after the save")
+//       res.json({"status" : "success"})
+//     }
+//     catch(error) {
+//       console.log("error: ", error);
+//       res.status(500).json({status: "error", error: error});
+//     }
+// });
   
 export default router;
