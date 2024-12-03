ed9519699c20929b7abd8f3ffc7b11c1b4448a36
Fan Bu <fannbuu@gmail.com>
2024-12-02 18:50:14 -0800

    test file templates

--- frontend/src/components/ResourcesPage.test.js
+++ frontend/src/components/ResourcesPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ResourcesPage from './ResourcesPage';
+import ResourcesPage from './ResourcesPage.js';
 
 test('renders ResourcesPage without crashing', () => {
   render(<ResourcesPage />);
---
+++ frontend/src/components/WaterTracker.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import WaterTracker from './WaterTracker.js';
+
+test('renders ResourcesPage without crashing', () => {
+  render(<WaterTracker />);
+// add your secefic test below
+
+});
